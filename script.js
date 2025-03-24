276e258314e2f759f2825c9528e4290712d42ac3
TrilhaX <joaotrilha01@gmail.com>
2025-03-24 08:28:59 -0300

    Adicionado o texto de pontos, Max pontos, texto de game over e o button de restart Game, sem funcionalidade ainda. Ja foi adicionado colisão a borda e mudado a cor da cobra (Verde) e da maça (Vermelha).

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('jogoCanvas')
 const ctx = canvas.getContext('2d')
+const gameOverDiv = document.querySelector('.gameOverDiv')
 const t = Math.random() * canvas.height
+let gameRunning = true
 
 const teclasPressionadas = {
     KeyW: false,
@@ -20,6 +22,11 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+function gameOver() {
+    if (gameRunning == false) {
+        gameOverDiv.style.display = "flex"
+    }
+}
 
 class Entidade {
     constructor(x, y, largura, altura) {
@@ -28,8 +35,8 @@ class Entidade {
         this.largura = largura
         this.altura = altura
     }
-    desenhar() {
-        ctx.fillStyle = 'black'
+    desenhar(cor) {
+        ctx.fillStyle = cor
         ctx.fillRect(this.x, this.y, this.largura, this.altura)
     }
 }
@@ -72,7 +79,8 @@ class Cobra extends Entidade {
             this.y < 0 ||
             this.y + this.altura > canvas.height
         ) {
-            console.log("Game Over");
+            gameRunning = false;
+            gameOver()
         }
     }
 }
@@ -81,19 +89,22 @@ class Comida extends Entidade {
         super(Math.random() * canvas.width - 10, t - 10, 20, 20)
     }
 }
-
 // o T foi declarado la em cima professor
 
 const cobra = new Cobra(100, 200, 20, 20)
 const comida = new Comida()
 
-
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    cobra.desenhar()
-    cobra.atualizar()
-    comida.desenhar()
-    cobra.verificarColisao(comida)
-    requestAnimationFrame(loop)
+    cobra.desenhar('Green')
+    comida.desenhar('Red')
+    if (gameRunning == true) {
+        cobra.desenhar('Green')
+        cobra.atualizar()
+        comida.desenhar('Red')
+        cobra.verificarColisao(comida)
+        cobra.verificarBorda()
+        requestAnimationFrame(loop)
+    }
 }
 loop()
